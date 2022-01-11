c0613bf0c16f53c53005d3ce64811a9906045cbb
Shantanu Maskeri <shaan.mask@gmail.com>
2022-01-11 12:05:18 +0400

    Space Shooter 2D Game

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour 
+{
+	public GameObject explosion;
+	public GameObject playerExplosion;
+
+	public int scoreValue;
+	private  GameController gameController;
+
+	void Start ()
+	{
+		GameObject gameControllerObj = GameObject.FindGameObjectWithTag ("GameController");
+		gameController = gameControllerObj.GetComponent<GameController>();	
+	}
+
+	void OnTriggerEnter (Collider col)
+	{
+		if (col.gameObject.CompareTag ("Boundary") || col.gameObject.CompareTag ("Enemy"))
+		{
+			return;
+		}
+
+		if (explosion != null)
+		{
+			Instantiate (explosion, transform.position, transform.rotation);	
+		}
+
+		if (col.gameObject.CompareTag ("Bolt"))
+		{
+			gameController.AddScore (scoreValue);	
+		}
+
+		if (col.gameObject.CompareTag ("Player"))
+		{
+			Instantiate (playerExplosion, col.gameObject.transform.position, col.gameObject.transform.rotation);
+			gameController.GameOver ();
+		}
+
+		Destroy (col.gameObject);
+		Destroy (gameObject);
+	}
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour 
+{
+	public GameObject[] hazards;
+	public Vector3 spawnValues;
+
+	public int hazardCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
+
+	public GUIText scoreText;
+	public GUIText restartText;
+	public GUIText gameOverText;
+	public GUIText waveText;
+
+	private bool gameOver;
+	private bool restart;
+	private int score;
+	private int waveCount;
+
+	void Start ()
+	{
+		gameOver = false;
+		restart = false;
+		restartText.text = "";
+		gameOverText.text = "";
+		score = 0;
+
+		UpdateScore ();
+		UpdateWaves ();
+	}
+
+	void Update ()
+	{
+		if (restart)
+		{
+			if (Input.GetKeyDown (KeyCode.R))
+			{
+				SceneManager.LoadScene ("Main");
+			}
+		}
+	}
+
+	void UpdateWaves ()
+	{
+		waveCount++;
+		waveText.text = "Wave: "+waveCount;
+
+		StartCoroutine (SpawnWaves ());
+	}
+
+	IEnumerator SpawnWaves ()
+	{
+		yield return new WaitForSeconds (startWait);
+
+		while (true)
+		{
+			for (int i = 0; i < hazardCount; i++)
+			{
+				waveText.text = "";
+				GameObject hazard = hazards[Random.Range (0, hazards.Length)];
+				Vector3 spawnPosition = new Vector3 (Random.Range (-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate (hazard, spawnPosition, spawnRotation);
+
+				yield return new WaitForSeconds (spawnWait);
+			}
+
+			yield return new WaitForSeconds (waveWait);
+
+			if (gameOver)
+			{
+				restartText.text = "Press 'R' to Restart";
+				restart = true;
+				break;
+			}
+			else
+			{
+				UpdateWaves ();
+				break;
+			}
+		}
+	}
+
+	public void AddScore (int scoreValue)
+	{
+		score += scoreValue;
+		UpdateScore ();
+	}
+
+	void UpdateScore ()
+	{
+		scoreText.text = "Score: "+score;
+	}
+
+	public void GameOver ()
+	{
+		gameOverText.text = "Game Over";
+		gameOver = true;
+	}
+}
